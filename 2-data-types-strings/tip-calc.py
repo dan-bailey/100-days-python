f3188173b2b7576532f744f9d2a4b9abc7d74038
Dan Bailey <dbailey@danbailey.net>
2023-12-28 12:42:13 -0600

    formatting after decimal

--- 2-data-types-strings/tip-calc.py
+++ 2-data-types-strings/tip-calc.py
@@ -5,5 +5,6 @@ people = input("How many people to split the bill? ")
 percentage_as_float = float(percentage) / 100
 totalBill = float(bill) * (1 + percentage_as_float)
 billPerPerson = round((totalBill / int(people)), 2)
+billPerPerson = "{:.2f}".format(billPerPerson)
 
 print(f"Each person should pay: ${billPerPerson}")
