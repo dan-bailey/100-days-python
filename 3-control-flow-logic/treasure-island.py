8b4dfaa741a085125cb34633c95619d44d865849
Dan Bailey <dbailey@danbailey.net>
2024-01-11 10:57:55 -0600

    skipped this

---
+++ 3-control-flow-logic/treasure-island.py
@@ -0,0 +1,4 @@
+# this is a basic chain of if/elif/else statements
+# i am skipping this one because it is incredibly rudimentary
+# and i have already done this countless times in everything
+# from Apple ][ BASIC to Javascript to Python
