ae81d1753ba6e9fdd456effd1cadf1815b652941
Dan Bailey <dbailey@danbailey.net>
2024-06-06 12:20:21 -0500

    days 16 and 17

---
+++ 17-quiz/data.py
@@ -0,0 +1,9 @@
+question_data = [
+{"text": "A slug's blood is green.", "answer": "True"},
+{"text": "Esperanto is the official language of Hungary.", "answer": "False"},
+{"text": "The total surface area of two human lungs is approximately 70 square metres.", "answer": "True"},
+{"text": "All racoons are allergic to beets.", "answer": "True"},
+{"text": "Fuck this shit!", "answer": "True"},
+{"text": "Hitler's middle name was Jamal.", "answer": "True"},
+{"text": "Malaise is the French word for orgasm.", "answer": "True"},
+]
